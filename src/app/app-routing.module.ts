6718c7053a535d02808c362084855c2df857f777
jilalat <ae.jilalat@pyxicom.com>
2022-12-26 10:19:03 +0100

    edit json pkg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { WawbComponent } from './maps/m1wawb/wawb.component';
 import { TroubledwatersComponent } from './maps/m3troubledwaters/troubledwaters.component';
+import { M6Component } from './maps/m6/m6.component';
 
 const routes: Routes = [
   {path: 'water-above-water-below', component: WawbComponent},
-  {path: 'troubled-waters-the-nile-conflict', component: TroubledwatersComponent},
+  { path: 'troubled-waters-the-nile-conflict', component: TroubledwatersComponent },
+  { path: 'm6', component: M6Component },
   {path: '', component: MainPageComponent}
 ];
 
